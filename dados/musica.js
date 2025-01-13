da3e7d186f1f1b94aee635f84793d173d1e85c91
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2025-01-13 11:32:12 -0300

    :bug: fix: decreasing audio volume

---
+++ dados/musica.js
@@ -0,0 +1,222 @@
+let dadosMusica = [
+    {
+        pergunta: "Qual é o nome do álbum mais vendido de todos os tempos?",
+        alternativas: [
+            "Thriller",
+            "Back in Black",
+            "The Dark Side of the Moon",
+            "Hotel California"
+        ],
+        respostaIndex: 0
+    },
+    {
+        pergunta: "Quem é conhecido como o 'Rei do Rock and Roll'?",
+        alternativas: [
+            "Chuck Berry",
+            "Little Richard",
+            "Buddy Holly",
+            "Elvis Presley"
+        ],
+        respostaIndex: 3
+    },
+    {
+        pergunta: "Qual banda britânica é famosa pelo álbum 'A Night at the Opera'?",
+        alternativas: [
+            "The Beatles",
+            "Queen",
+            "Led Zeppelin",
+            "The Rolling Stones"
+        ],
+        respostaIndex: 1
+    },
+    {
+        pergunta: "Quem compôs a famosa peça 'A Primavera' do 'As Quatro Estações'?",
+        alternativas: [
+            "Johann Sebastian Bach",
+            "Ludwig van Beethoven",
+            "Antonio Vivaldi",
+            "Wolfgang Amadeus Mozart"
+        ],
+        respostaIndex: 2
+    },
+    {
+        pergunta: "Qual é o nome verdadeiro do cantor conhecido como 'Prince'?",
+        alternativas: [
+            "Prince Rogers Nelson",
+            "Michael Joseph Jackson",
+            "Stefani Joanne Angelina Germanotta",
+            "David Robert Jones"
+        ],
+        respostaIndex: 0
+    },
+    {
+        pergunta: "Qual cantor é famoso pela música 'Like a Rolling Stone'?",
+        alternativas: [
+            "Neil Young",
+            "Bob Dylan",
+            "Bruce Springsteen",
+            "Johnny Cash"
+        ],
+        respostaIndex: 1
+    },
+    {
+        pergunta: "Qual é o nome da cantora que lançou o álbum '25' em 2015?",
+        alternativas: [
+            "Taylor Swift",
+            "Beyoncé",
+            "Adele",
+            "Lady Gaga"
+        ],
+        respostaIndex: 2
+    },
+    {
+        pergunta: "Qual estilo musical tem como um dos seus pioneiros o músico Louis Armstrong?",
+        alternativas: [
+            "Jazz",
+            "Blues",
+            "Rock",
+            "Country"
+        ],
+        respostaIndex: 0
+    },
+    {
+        pergunta: "Quem é o artista por trás da música 'Bohemian Rhapsody'?",
+        alternativas: [
+            "Elton John",
+            "David Bowie",
+            "Paul McCartney",
+            "Freddie Mercury"
+        ],
+        respostaIndex: 3
+    },
+    {
+        pergunta: "Qual banda lançou o álbum 'Nevermind' em 1991?",
+        alternativas: [
+            "Pearl Jam",
+            "Nirvana",
+            "Alice in Chains",
+            "Soundgarden"
+        ],
+        respostaIndex: 1
+    },
+    {
+        pergunta: "Qual é o nome do estilo musical que surgiu na Jamaica na década de 1960?",
+        alternativas: [
+            "Reggae",
+            "Ska",
+            "Dancehall",
+            "Dubstep"
+        ],
+        respostaIndex: 0
+    },
+    {
+        pergunta: "Quem é conhecido como o 'Pai do Blues'?",
+        alternativas: [
+            "Muddy Waters",
+            "B.B. King",
+            "Robert Johnson",
+            "Howlin' Wolf"
+        ],
+        respostaIndex: 2
+    },
+    {
+        pergunta: "Qual cantora ganhou o Grammy de Álbum do Ano por 'Fearless' em 2010?",
+        alternativas: [
+            "Taylor Swift",
+            "Katy Perry",
+            "Lady Gaga",
+            "Rihanna"
+        ],
+        respostaIndex: 0
+    },
+    {
+        pergunta: "Qual artista é conhecido por seu estilo musical 'Folk Rock' e pelo álbum 'Harvest'?",
+        alternativas: [
+            "James Taylor",
+            "Neil Young",
+            "Simon & Garfunkel",
+            "Bob Dylan"
+        ],
+        respostaIndex: 1
+    },
+    {
+        pergunta: "Qual banda lançou o álbum 'Dark Side of the Moon' em 1973?",
+        alternativas: [
+            "Genesis",
+            "The Who",
+            "Yes",
+            "Pink Floyd"
+        ],
+        respostaIndex: 3
+    },
+    {
+        pergunta: "Qual artista é famoso por seu trabalho no estilo 'Psychedelic Rock' e pelo álbum 'Sgt. Pepper's Lonely Hearts Club Band'?",
+        alternativas: [
+            "The Doors",
+            "The Beatles",
+            "Jimi Hendrix",
+            "The Byrds"
+        ],
+        respostaIndex: 1
+    },
+    {
+        pergunta: "Quem é o artista por trás da música 'Purple Haze'?",
+        alternativas: [
+            "Jimi Hendrix",
+            "Eric Clapton",
+            "Jimmy Page",
+            "Jeff Beck"
+        ],
+        respostaIndex: 0
+    },
+    {
+        pergunta: "Qual é o nome do álbum de 1979 que é considerado um dos maiores álbuns de punk rock?",
+        alternativas: [
+            "London Calling",
+            "Never Mind the Bollocks",
+            "Ramones",
+            "Back in Black"
+        ],
+        respostaIndex: 1
+    },
+    {
+        pergunta: "Qual banda é conhecida por sua música 'Hotel California'?",
+        alternativas: [
+            "Fleetwood Mac",
+            "The Rolling Stones",
+            "The Who",
+            "Eagles"
+        ],
+        respostaIndex: 3
+    },
+    {
+        pergunta: "Qual artista é famoso pela música 'Viva La Vida'?",
+        alternativas: [
+            "U2",
+            "Radiohead",
+            "Coldplay",
+            "Muse"
+        ],
+        respostaIndex: 2
+    },
+    {
+        pergunta: "Qual é o nome verdadeiro da cantora conhecida como 'Madonna'?",
+        alternativas: [
+            "Madonna Louise Ciccone",
+            "Stefani Joanne Angelina Germanotta",
+            "Gwen Stefani",
+            "Cyndi Lauper"
+        ],
+        respostaIndex: 0
+    },
+    {
+        pergunta: "Qual é o nome do álbum lançado pelo artista Michael Jackson em 1982?",
+        alternativas: [
+            "Thriller",
+            "Bad",
+            "Dangerous",
+            "HIStory"
+        ],
+        respostaIndex: 0
+    }
+];
